9271f0e5bfcd153c32b5d1131fad2df6147c0b47
pawanharariya <pawanharariya@gmail.com>
2023-07-09 01:45:52 +0530

    Add vector drawables for dice numbers

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton : Button = findViewById(R.id.roll_button)
+        val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
     }
 
     private fun rollDice() {
-        val displayText : TextView = findViewById(R.id.display_text)
-        val randomNumber = Random.nextInt(6) + 1;
-        displayText.text = randomNumber.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val imageResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(imageResource)
     }
 }

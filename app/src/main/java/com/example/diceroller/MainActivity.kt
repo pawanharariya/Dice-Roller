2a70cd7c8d3397bc0f8066769d669082dee3dc5f
pawanharariya <pawanharariya@gmail.com>
2023-07-09 01:00:43 +0530

    Add Roll button to generates a random number between 1 and 6

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+    }
+
+    private fun rollDice() {
+        val displayText : TextView = findViewById(R.id.display_text)
+        val randomNumber = Random.nextInt(6) + 1;
+        displayText.text = randomNumber.toString()
     }
 }
